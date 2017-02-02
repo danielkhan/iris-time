bb6db89a0f38ddd2da7ebd6a948cd6b47ddc9091
dkhan <datenpunk@gmail.com>
2017-02-02 21:23:40 +0100

    Minor fixes

--- config/index.js
+++ config/index.js
@@ -1,6 +1,21 @@
 require('dotenv').config();
 
+const bunyan = require('bunyan');
+
+const log = {
+    development: bunyan.createLogger({ name: 'IRIS-time-development', level: 'debug' }),
+    production: bunyan.createLogger({ name: 'IRIS-time-production', level: 'info' }),
+    // We don't want to have logging output in our test output - so go for only fatal errors
+    test: bunyan.createLogger({ name: 'IRIS-time-test', level: 'fatal' }),
+
+};
+
 module.exports = {
     googleTimeApiKey: process.env.GOOGLE_TIME_API_KEY,
     googleGeoApiKey: process.env.GOOGLE_GEO_API_KEY,
-}
+
+    log: (env) => {
+        if (env) return log[env];
+        return log[process.env.NODE_ENV || 'development'];
+    }
+};
--- test/server/service.test.js
+++ test/server/service.test.js
@@ -1,30 +1,34 @@
 'use strict';
 
-const should = require('should');
+require('should');
 const request = require('supertest');
-const service = require('../../server/service');
+
+const config = require('../../config');
+const log = config.log();
+const service = require('../../server/service')(config);
 
 describe('The express service', () => {
-  describe('GET /', () => {
-    it('should return http/404', (done) => {
-      request(service)
-      .get('/')
-      .expect(404, done);
+    describe('GET /', () => {
+        it('should return http/404', (done) => {
+            request(service)
+            .get('/')
+            .expect(404, done);
+        });
     });
-  });
 
-  describe('GET /service/location', () => {
-    it('should return HTTP:200 and reply with a valid result', (done) => {
-      request(service)
-      .get('/service/vienna')
-      .expect(200)
-      .end((err, res) => {
-        if(err) return done(err);
-        // here we are using should to make an assertion
-        res.body.result.should.exist;
-        return done();
+    describe('GET /service/location', () => {
+        it('should return HTTP:200 and reply with a valid result', (done) => {
+            request(service)
+            .get('/service/vienna')
+            .expect(200)
+            .end((err, res) => {
+            
+                if (err) return done(err);
+                // here we are using should to make an assertion
+                res.body.result.should.exist;
+                return done();
 
-      });
+            });
+        });
     });
-  });
 });
