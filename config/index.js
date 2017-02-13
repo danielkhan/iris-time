245e6b55dbc2a96f64ce4e4c48146ae79d5ea035
dkhan <datenpunk@gmail.com>
2017-02-12 21:47:15 -0800

    New code

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const bunyan = require('bunyan');
-
+const serviceAccessToken = require('crypto').randomBytes(Math.ceil(32/2)).toString('hex').slice(0,32);
 const log = {
     development: bunyan.createLogger({ name: 'IRIS-time-development', level: 'debug' }),
     production: bunyan.createLogger({ name: 'IRIS-time-production', level: 'info' }),
@@ -14,6 +14,10 @@ module.exports = {
     googleTimeApiKey: process.env.GOOGLE_TIME_API_KEY,
     googleGeoApiKey: process.env.GOOGLE_GEO_API_KEY,
 
+
+    serviceAccessToken: serviceAccessToken,
+
+    irisApiToken: process.env.IRIS_API_TOKEN,
     log: (env) => {
         if (env) return log[env];
         return log[process.env.NODE_ENV || 'development'];
--- test/server/service.test.js
+++ test/server/service.test.js
@@ -4,31 +4,43 @@ require('should');
 const request = require('supertest');
 
 const config = require('../../config');
-const log = config.log();
 const service = require('../../server/service')(config);
 
 describe('The express service', () => {
     describe('GET /', () => {
         it('should return http/404', (done) => {
             request(service)
-            .get('/')
-            .expect(404, done);
+                .get('/')
+                .expect(404, done);
         });
     });
 
     describe('GET /service/location', () => {
+        it('should return HTTP:403 if called without an access token', (done) => {
+            request(service)
+                .get('/service/vienna')
+                .expect(403)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    // here we are using should to make an assertion
+                    res.body.should.be.empty;
+                    return done();
+                });
+        });
+
         it('should return HTTP:200 and reply with a valid result', (done) => {
             request(service)
-            .get('/service/vienna')
-            .expect(200)
-            .end((err, res) => {
-            
-                if (err) return done(err);
-                // here we are using should to make an assertion
-                res.body.result.should.exist;
-                return done();
+                .get('/service/vienna')
+                .set('X-IRIS-SERVICE-TOKEN', config.serviceAccessToken)
+                .expect(200)
+                .end((err, res) => {
 
-            });
+                    if (err) return done(err);
+                    // here we are using should to make an assertion
+                    res.body.result.should.exist;
+                    return done();
+
+                });
         });
     });
 });
