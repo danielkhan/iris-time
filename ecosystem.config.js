ce1d2d0924acdb652a7c5bbaca579d4287563af9
dkhan <datenpunk@gmail.com>
2017-02-15 18:41:41 -0800

    added pm2 deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    apps: [
+        {
+            name: 'IRIS-TIME',
+            script: 'bin/run.js',
+            env_production: {
+                NODE_ENV: 'production',
+                IRIS_URL: 'http://34.251.16.136:3000/'
+            }
+        }
+    ],
+    deploy: {
+        production: {
+            user: 'node',
+            host: ['34.251.16.119', '34.250.101.115'],
+            ref: 'origin/master',
+            repo: 'git@github.com:danielkhan/iris-time.git',
+            path: '/var/srv/production',
+            'post-deploy': 'cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production'
+        }
+    }
+};
