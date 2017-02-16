037c791b705c8a20b7facf017075cdc7165aa306
dkhan <datenpunk@gmail.com>
2017-02-15 18:56:48 -0800

    fixed iris url

--- bin/run.js
+++ bin/run.js
@@ -10,10 +10,12 @@ const server = http.createServer(service);
 server.listen(process.env.PORT || null);
 
 server.on('listening', function () {
-    console.log(process.env.IRIS_URL);
     log.info(`IRIS-Time is listening on ${server.address().port} in ${service.get('env')} mode.`);
+
+    const irisUrl = process.env.IRIS_URL || 'http://127.0.0.1:3000';
+
     const announce = () => {
-        request.put(`{process.env.IRIS_URL || http://127.0.0.1:3000}/service/time/${server.address().port}`)
+        request.put(`${irisUrl}/service/time/${server.address().port}`)
             .set('X-IRIS-SERVICE-TOKEN', config.serviceAccessToken)
             .set('X-IRIS-API-TOKEN', config.irisApiToken)
             .end((err) => {
